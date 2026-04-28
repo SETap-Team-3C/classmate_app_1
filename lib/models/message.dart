06327eb1cc01f560aa2ee7c0a69ee1de5a26045a
rishutonk02 <rishutonk02@gmail.com>
2026-04-28 15:23:08 +0100

    add

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,24 +1,60 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
+  final String id;
+  final String chatId;
   final String senderId;
   final String receiverId;
   final String text;
-  final Timestamp timestamp;
+  final Timestamp? timestamp;
+  final bool read;
+  final Timestamp? readAt;
+  final String? readBy;
 
   Message({
+    this.id = '',
+    this.chatId = '',
     required this.senderId,
     required this.receiverId,
     required this.text,
-    required this.timestamp,
+    this.timestamp,
+    this.read = false,
+    this.readAt,
+    this.readBy,
   });
 
-  Map<String, dynamic> toMap() {
+  factory Message.fromMap(String id, Map<String, dynamic> data) {
+    return Message(
+      id: id,
+      chatId: (data['chatId'] ?? '').toString(),
+      senderId: (data['senderId'] ?? '').toString(),
+      receiverId: (data['receiverId'] ?? '').toString(),
+      text: (data['text'] ?? '').toString(),
+      timestamp: data['timestamp'] is Timestamp
+          ? data['timestamp'] as Timestamp
+          : null,
+      read: data['read'] == true,
+      readAt: data['readAt'] is Timestamp ? data['readAt'] as Timestamp : null,
+      readBy: data['readBy']?.toString(),
+    );
+  }
+
+  factory Message.fromDocument(
+    QueryDocumentSnapshot<Map<String, dynamic>> doc,
+  ) {
+    return Message.fromMap(doc.id, doc.data());
+  }
+
+  Map<String, dynamic> toCreateMap() {
     return {
+      'chatId': chatId,
       'senderId': senderId,
       'receiverId': receiverId,
       'text': text,
-      'timestamp': timestamp,
+      'timestamp': FieldValue.serverTimestamp(),
+      'read': read,
+      'readAt': readAt,
+      'readBy': readBy,
     };
   }
 }
