4bb1f3a1bcc9c511ef19f4fb7f05ee0d88ab553a
Fardeen shaikh <fardeen6177@gmail.com>
2026-04-28 17:07:53 +0100

    add dark mode

---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDarkMode = false;
+  late SharedPreferences _prefs;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _initTheme();
+  }
+
+  Future<void> _initTheme() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkMode = _prefs.getBool('isDarkMode') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    await _prefs.setBool('isDarkMode', _isDarkMode);
+    notifyListeners();
+  }
+
+  Future<void> setDarkMode(bool value) async {
+    _isDarkMode = value;
+    await _prefs.setBool('isDarkMode', _isDarkMode);
+    notifyListeners();
+  }
+
+  ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+
+  ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
