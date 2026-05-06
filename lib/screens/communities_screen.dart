f93eeb6678408566ad3720a7e39804f7342e0414
leo-mack <leo.m4ck@gmail.com>
2026-05-06 20:03:54 +0100

    added CommunitiesScreen for displaying community information with a placeholder

---
+++ lib/screens/communities_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommunitiesScreen extends StatelessWidget {
+  const CommunitiesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Communities')),
+      body: const Center(
+        child: Text('Communities placeholder'),
+      ),
+    );
+  }
+}
